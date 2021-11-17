d4f5364d6d175984a0214e8f832be85222cd6a12
Attlas2 <antoniokastoun4@gmail.com>
2021-11-17 12:19:41 +0100

    Created folders and established basic structure.

--- src/js/includes/script.js
+++ src/js/includes/script.js
@@ -5,12 +5,12 @@ function addMilk() {
         document.getElementById("milkcounter").innerHTML=Math.round(milk/100)/10+"K Milch"
     }
 }
-function addBauer(){
-    if (milk>10){
-        milk-=10;
-        setInterval(addMilk,1000);
+function addBauer() {
+    if (milk > 10) {
+        milk -= 10;
+        setInterval(addMilk, 1000);
         bauer++;
-        document.getElementById("bauerncounter").innerHTML=bauer+" Bauern"
+        document.getElementById("bauerncounter").innerHTML = bauer + " Bauern"
     }
 
 

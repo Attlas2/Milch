a6bb414ce887526e7b0fa8fd6dd02da447484d2b
Niloc <colinseelbach@gmail.com>
2022-03-03 13:52:45 +0100

    Moved Classes
    Moved Classes to new files and made small structure changes

--- src/js/classes/Milchproduktion.js
+++ src/js/classes/Milchproduktion.js
@@ -75,38 +75,3 @@ class Milchproduktion {
     }
 
 }
-class Achievements{
-    milestone;
-    id;
-    klasse;
-    message;
-    achieved;
-
-    constructor(milestone,id,klasse,message,) {
-        this.milestone=milestone;
-        this.id=id;
-        this.klasse=klasse;
-        this.message=message;
-        this.achieved=false;
-
-    }
-    checkStone(x){
-        if(!this.achieved){
-            if(x>=this.milestone){
-                this.achieved=true;
-                document.getElementById(this.klasse).style.background = 'url("../assets/img/ui_assets/menu/menu_achivement_completed.png") no-repeat center' // Hier den Pfad von dem anderen Bild.
-                document.getElementById(this.klasse).style.backgroundSize = "100% 100%";
-                document.getElementById(this.id).innerHTML=this.message
-            }
-        }
-    }
-    loadAchieve(a){
-        if(a){
-            document.getElementById(this.klasse).style.background = 'url("../assets/img/ui_assets/menu/menu_achivement_completed.png") no-repeat center' // Hier den Pfad von dem anderen Bild.
-            document.getElementById(this.klasse).style.backgroundSize = "100% 100%";
-            document.getElementById(this.id).innerHTML=this.message
-        }
-    }
-}
-
-
